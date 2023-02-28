4d70456037a3781c95e053e3bb118bd70cbcfb08
Humza <109144839+humzaco@users.noreply.github.com>
2023-02-28 10:47:40 +0500

    Profile Image Chnaged

--- main.js
+++ main.js
@@ -33,35 +33,3 @@ new Chart(chart, {
         responsive: true
     }
 })
-
-
-
-
-
-// Show or hide sidebar
-const menuBtn = document.querySelector('#menu-btn');
-const closeBtn = document.querySelector('#close-btn');
-const sidebar = document.querySelector('aside.flex');
-
-menuBtn.addEventListener('click', () => {
-    sidebar.style.display ='block';
-})
-
-closeBtn.addEventListener('click', () => {
-    sidebar.style.display = 'none';
-})
-
-
-
-
-
-// Change Theme
-const themeBtn = document.querySelector('.theme-btn');
-
-themeBtn.addEventListener('click', () => {
-    document.body.classList.toggle('dark-theme');
-
-    themeBtn.querySelector('span:first-child').classList.toggle('active');
-    themeBtn.querySelector('span:last-child').classList.toggle('active');
-})
-
